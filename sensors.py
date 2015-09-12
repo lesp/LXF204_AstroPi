771aa4e6a2f5f0282dbc34d3f4aa481f56fde7f8
Les Pounder <lespounder@gmail.com>
2015-09-12 15:08:47 +0000

    Updated

--- sensors.py
+++ sensors.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from sense_hat import SenseHat
+import time
 
 sense = SenseHat()
 
@@ -11,23 +12,43 @@ pygame.display.set_mode((640,480))
 
 def joystick(event):
     if event.key == pygame.K_DOWN:
-        print("DOWN")
-        temperature()
+        invader()
     elif event.key == pygame.K_UP:
-        print("UP")
-        humidity()
+        pressure()
     elif event.key == pygame.K_LEFT:
-        print("LEFT")
+        temperature()
     elif event.key == pygame.K_RIGHT:
-        print("RIGHT")
+        humidity()
 
 def temperature():
-    temp = sense.get_temperature()
+    temp = round(sense.get_temperature(),1)
     sense.show_message("The temperature is %s C" % temp)
 
 def humidity():
-    humid = sense.get_humidity()
-    sense.show_message("Humidity: %s %%rH" % humidity)
+    humid = round(sense.get_humidity(),1)
+    sense.show_message("Humidity: %s %%" % humid)
+
+def pressure():
+    pressure = round(sense.get_pressure(),1)
+    sense.show_message("Pressure %s Millibars" % pressure)
+
+def invader():
+    x = [0,255,0]
+    for i in range(3):
+        o = [0,0,0]
+        invader = [
+            o,x,o,o,o,o,x,o,
+            o,o,x,o,o,x,o,o,
+            o,x,x,x,x,x,x,o,
+            x,x,o,x,x,o,x,x,
+            x,x,x,x,x,x,x,x,
+            x,o,x,x,x,x,o,x,
+            x,o,x,o,o,x,o,x,
+            o,o,x,x,x,x,o,o,
+        ]
+        sense.set_pixels(invader)
+        time.sleep(2)
+        x = [255,0,0]
 
 sense.low_light = True
 while True:
